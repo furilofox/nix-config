8ae56cf054eaae48e176a8f27e17609fd3d867a6
Furilo <53122773+furilofox@users.noreply.github.com>
2025-04-29 09:34:35 +0200

    remove steam from VM

--- hosts/gehenna/default.nix
+++ hosts/gehenna/default.nix
@@ -33,26 +33,6 @@
     curl
   ];
 
-  # Gaming Stuff
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
-  };
-
-  programs.gamemode.enable = true;
-
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
-  };
-  
-
   # System version
   system.stateVersion = "24.11";
 }
