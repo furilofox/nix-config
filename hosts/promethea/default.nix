2fe3db6562d857bed3806caf05b1d224076e8b28
Furilo <53122773+furilofox@users.noreply.github.com>
2025-05-02 15:18:03 +0200

    use latest kernel

--- hosts/promethea/default.nix
+++ hosts/promethea/default.nix
@@ -16,6 +16,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "promethea"; # Define your hostname.
 
